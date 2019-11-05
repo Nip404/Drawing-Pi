8d9b1c85c655c193dfe1cab6b3fec22da3c2d9d7
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-05 23:43:22 +0000

    Update and rename main.py to drawing_pi.py

--- drawing_pi.py
+++ drawing_pi.py
@@ -3,26 +3,26 @@ import math
 import sys
 import random
 
-s = [1000,800]
+s = [1000, 800]
 
 pygame.init()
-screen = pygame.display.set_mode(s,0,32)
+screen = pygame.display.set_mode(s, 0, 32)
 pygame.display.set_caption("Pi to 1 billion digits")
 
 current = [int(i/2) for i in s]
 magnification = 0.8
 
-pairs = {i:i*36 for i in range(10)}
-colours = {i:(random.randint(0,255),random.randint(0,255),random.randint(0,255)) for i in range(10)}
-font = pygame.font.SysFont("Garamond MS",20)
+pairs = {i: i*36 for i in range(10)}
+colours = {i: tuple(random.randint(0, 255) for _ in range(3)) for _ in range(10)}
+font = pygame.font.SysFont("Garamond MS", 20)
 
-screen.fill((0,0,0))
+screen.fill((0, 0, 0))
 
 def heading(deg):
-    return [math.cos(math.radians(deg)),math.sin(math.radians(deg))]
+    return [math.cos(math.radians(deg)), math.sin(math.radians(deg))]
 
-with open("digits.txt","r") as f:
-    for pos,digit in enumerate(f.read()):
+with open("digits.txt", "r") as f:
+    for pos, digit in enumerate(f.read()):
         try:
             digit = int(digit)
         except:
@@ -33,12 +33,12 @@ with open("digits.txt","r") as f:
                 pygame.quit()
                 sys.exit()
 
-        pygame.draw.rect(screen,(0,00,0),[0,0,200,25],0)
-        msg = font.render(f"Digit {pos}: {digit}",True,(255,255,255))
-        screen.blit(msg,(10,10))
+        pygame.draw.rect(screen, (0, 0, 0), [0, 0, 200, 25], 0)
+        msg = font.render(f"Digit {pos}: {digit}", True, (255, 255, 255))
+        screen.blit(msg, (10, 10))
         
         new = [current[i] + [j*2 for j in heading(pairs[int(digit)])][i] for i in range(2)]
-        pygame.draw.line(screen,colours[int(digit)],current,new,1)
+        pygame.draw.line(screen, colours[int(digit)], current, new, 1)
         current = new
 
         pygame.display.flip()
